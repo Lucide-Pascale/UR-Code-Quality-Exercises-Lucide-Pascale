d1efd6954616b1393d9904d65dae814458998b11
Lucide-Pascale <lucpas15nov@gmail.com>
2026-04-28 18:02:26 +0200

    Adding Question 2 in Exercise 3

---
+++ exercise-3/question2.js
@@ -0,0 +1,12 @@
+// Write a JavaScript function to check if a given number is prime or not.
+function isPrime(num) {
+    if (num <= 1) {
+        return false; // 0 and 1 are not prime numbers
+    }
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            return false; // num is divisible by i, so it's not prime
+        }
+    }
+    return true; // num is prime
+}
